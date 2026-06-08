4382e4ca858ef442cc0038fc72d2d150a65f0092
ed <ed@beee.dev>
2026-06-08 09:15:57 +0000

    reg-info: move registration deadline to just below first point
    Moved 'Registration closes on June 18, 2026...' to position 2, keeping all other points in their original order.

--- src/components/RegistrationForm.svelte
+++ src/components/RegistrationForm.svelte
@@ -214,10 +214,10 @@
 			<ul class="space-y-1 text-[14px] leading-snug text-black font-registration">
 				<li class="border-l-2 border-primary pl-3">Qualification slots are limited and will be allocated on a first-completed-registration basis.</li>
 				<li class="border-l-2 border-primary pl-3">Registration is through participating schools within the FCT.</li>
+				<li class="border-l-2 border-primary pl-3">Registration closes on June 18, 2026, or earlier if available placement slots are filled.</li>
 				<li class="border-l-2 border-primary pl-3">Participants must be between 10 and 14 years of age.</li>
 				<li class="border-l-2 border-primary pl-3">Each participating school must register four (4) players.</li>
 				<li class="border-l-2 border-primary pl-3">Sponsorship of participants is by parents or other interested sponsor.</li>
-				<li class="border-l-2 border-primary pl-3">Registration closes on June 18, 2026, or earlier if available placement slots are filled.</li>
 			</ul>
 
 			<p class="mt-4 text-[13px] font-registration text-muted">Questions? Email <a href="mailto:beee@apexlinks.org" class="text-primary underline-offset-2 hover:underline">beee@apexlinks.org</a> or call <span class="text-primary">+234 800 BEEE</span></p>
