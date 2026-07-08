f0951a3a5383a307d37b7bc1ab68f678197bde27
ed <ed@beee.dev>
2026-07-08 10:17:55 +0100

    constrain hero image to max-w-md so it appears smaller within its column

--- src/lib/components/championship/ChampHero.svelte
+++ src/lib/components/championship/ChampHero.svelte
@@ -113,8 +113,8 @@
       </div>
     </div>
 
-    <div class="lg:col-span-7" bind:this={imageWrapper}>
-      <div class="relative w-full aspect-square lg:aspect-[4/5] rounded-3xl overflow-visible">
+    <div class="lg:col-span-7 flex justify-center" bind:this={imageWrapper}>
+      <div class="relative w-full max-w-md aspect-square lg:aspect-[4/5] rounded-3xl overflow-visible">
         <img
           src="/images/hero.png"
           alt="Student playing chess"
